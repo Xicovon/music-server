bfb36ed43d2299b26e498dc7ce2ccfafb47e5914
Xicovon <bowen.goldade@gmail.com>
2025-01-31 09:27:58 -0600

    Added thumbnail download

--- app/sidekiq/download_song_job.rb
+++ app/sidekiq/download_song_job.rb
@@ -1,5 +1,6 @@
 class DownloadSongJob
   include Sidekiq::Job
+  require 'open-uri'
 
   OUTPUT_DIRECTORY = "/media/bowen/musicserver_data"
 
@@ -18,11 +19,18 @@ class DownloadSongJob
       channel_title = response.dig("items").first.dig("snippet", "channelTitle")
       title = response.dig("items").first.dig("snippet", "title")
       video_id = response.dig("items").first.dig("id")
+      output_path = "#{OUTPUT_DIRECTORY}/#{video_id}"
 
-      output_path = "#{OUTPUT_DIRECTORY}/#{song.video_id}"
+      # Skip if the file has already been downloaded
+      if File.file?("#{output_path}.opus")
+        YoutubeApi.delete_playlist_item(song.playlist_item_id)
+        song.destroy
+        next
+      end
 
       state = YoutubeDL.download("https://www.youtube.com/watch?v=#{song.video_id}", extract_audio: true, output: output_path).call
       if state.error?
+        File.delete(state.info_json)
         next
       end
 
@@ -32,8 +40,16 @@ class DownloadSongJob
         s = Song.new(video_id: video_id, original_title: title, title: title, path: "#{output_path}.opus", uploader: channel_title)
         s.save
 
+        download_thumbnail(song.thumbnail_url, "#{output_path}.jpg")
+
+        YoutubeApi.delete_playlist_item(song.playlist_item_id)
         song.destroy
       end
     end
+  end
+
+  def download_thumbnail(url, output)
+    download = URI.open(url)
+    IO.copy_stream(download, output)
   end
 end
